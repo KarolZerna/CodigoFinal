a988159c763dba41f463ce06cbaad2b9868f4f40
Daniel Acero <edanielacero@gmail.com>
2021-09-28 02:18:52 -0400

    rename field to match regular expressions and removed the unused local variable posicion

--- pacman/balance/RLObjects.py
+++ pacman/balance/RLObjects.py
@@ -3,7 +3,7 @@ import math
 class Accion:
     DERECHA = 1
     IZQUIERDA = 0
-    maxValor = 1
+    max_valor = 1
 
 
 class Posicion:
@@ -14,18 +14,17 @@ class Posicion:
 
 class Estado:
     angulo = 0
-    velocidadAngular = 0
+    velocidad_angular = 0
     posicion = Posicion.CENTRO
 
 
     def __init__(self):
         self.angulo = math.pi
-        self.velocidadAngular = 0
-        posicion = 0
+        self.velocidad_angular = 0
 
     def __repr__(self):
         return "Angulo: {}, Velocidad Angular: {}, Posicion: {})".format(
             self.angulo,
-            self.velocidadAngular,
+            self.velocidad_angular,
             self.posicion)
         
